b4baf400825b863b4fe57c0a8079fb087c00b62f
sergei_petriankin <Sergei_petriankin@epam.com>
2020-04-20 19:43:13 +0300

    search by first name or last name added to StudentsView

--- src/main/java/com/family/ui/view/list/StudentsView.java
+++ src/main/java/com/family/ui/view/list/StudentsView.java
@@ -5,7 +5,10 @@ import com.family.backend.service.StudentService;
 import com.family.ui.MainLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -16,6 +19,7 @@ public class StudentsView extends VerticalLayout {
     private StudentService studentService;
 
     private Grid<Student> grid = new Grid<>(Student.class);
+    private TextField filterText = new TextField();
 
     public StudentsView(StudentService studentService) {
         this.studentService = studentService;
@@ -28,7 +32,7 @@ public class StudentsView extends VerticalLayout {
         content.setSizeFull();
 
         // Adds the content layout to the main layout.
-        add(content);
+        add(getToolbar(), content);
         updateList();
     }
 
@@ -40,6 +44,16 @@ public class StudentsView extends VerticalLayout {
     }
 
     private void updateList() {
-        grid.setItems(studentService.findAll());
+        grid.setItems(studentService.findAll(filterText.getValue()));
+    }
+
+    private HorizontalLayout getToolbar() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
+        HorizontalLayout toolbar = new HorizontalLayout(filterText);
+        toolbar.addClassName("toolbar");
+        return toolbar;
     }
 }
