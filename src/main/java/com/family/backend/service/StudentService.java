4533838f0077304ad1b38542275f46da3b1a42b5
sergei_petriankin <Sergei_petriankin@epam.com>
2020-04-20 19:15:53 +0300

    StudentService fix test data provided in postconstruct

--- src/main/java/com/family/backend/service/StudentService.java
+++ src/main/java/com/family/backend/service/StudentService.java
@@ -5,13 +5,17 @@ import com.family.backend.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class StudentService {
 
-    StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
 
     public Student save(Student student) {
         return studentRepository.save(student);
@@ -23,5 +27,24 @@ public class StudentService {
 
     public void delete(Student student) {
         studentRepository.delete(student);
+    }
+
+    @PostConstruct
+    public void populateTestData() {
+        if (studentRepository.count() == 0) {
+            Random r = new Random(0);
+            studentRepository.saveAll(
+                    Stream.of("Ivan Ivanov", "Petr Petrov", "Sidor Sidorov",
+                            "John Doe", "Jane Doe", "Lorem Ipsum")
+                            .map(name -> {
+                                String[] split = name.split(" ");
+                                Student student = new Student();
+                                student.setFirstName(split[0]);
+                                student.setLastName(split[1]);
+                                student.setEducationType(Student.EducationType.values()[r.nextInt(Student
+                                        .EducationType.values().length)]);
+                                return student;
+                            }).collect(Collectors.toList()));
+        }
     }
 }
